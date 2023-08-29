c7945e17dee5c0bc5a5815b61a5a59f5c17960f9
Pankaj Dahale <pankajdahale77030@gmail.com>
2023-08-29 17:24:12 +0530

    creating jupitar notebook in jc penny

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 def main():
     print("Starting model execution...")
     
-    print("Model is under process .....")
+    print("Model is under process in vertix-ai .....")
     # Your model code goes here
     # For example, you can load data, train a model, make predictions, etc.
     
