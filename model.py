07285db995c75b2b25cec413e524f53d51cc8215
Pankaj Dahale <pankajdahale77030@gmail.com>
2023-08-29 15:22:27 +0530

    vertix-ai

---
+++ model.py
@@ -0,0 +1,13 @@
+# model.py
+
+def main():
+    print("Starting model execution...")
+    
+    print("Model is under process .....")
+    # Your model code goes here
+    # For example, you can load data, train a model, make predictions, etc.
+    
+    print("Model execution completed.")
+
+if __name__ == "__main__":
+    main()
